81bfb02874567c907fada8754a0bea361e86fd8f
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-10-24 16:43:10 +0530

    flatMappingExample

--- src/main/java/hello/ExploreCollection.java
+++ src/main/java/hello/ExploreCollection.java
@@ -146,6 +146,20 @@ public class ExploreCollection {
         print.accept(result);
     }
 
+    public void flatMappingExample(){
+        List<Integer> list1 = Arrays.asList(1,2,3);
+        List<Integer> list2 = Arrays.asList(4,5,6);
+        List<Integer> list3 = Arrays.asList(7,8,9);
+
+        List<List<Integer>> listOfLists = Arrays.asList(list1, list2, list3);
+
+        List<Integer> result = listOfLists.stream()
+                .flatMap(x -> x.stream())
+                .collect(Collectors.toList());
+        print.accept(result);
+    }
+
+
 }
 
 
