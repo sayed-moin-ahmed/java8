7acc9bb445eaedf8c61263d592ed8b4a9f30dca5
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-11-04 15:19:40 +0530

    Create Application.java

---
+++ src/main/java/java15/textblocks/Application.java
@@ -0,0 +1,16 @@
+package java15.textblocks;
+
+public class Application {
+
+    /**
+     *  Text block : """     """
+     * @param args
+     */
+    public static void main(String[] args){
+        String textBlocks= """
+                Hello world""";
+        System.out.println(textBlocks);
+    }
+
+
+}
